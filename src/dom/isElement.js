21a7f514e0e76d1f14305dc7f346bf5ed7b837ae
Fabien Motte <contact@fabienmotte.com>
2019-10-05 18:28:54 +0200

    fix(isElement): SVG element returns false

--- src/dom/isElement.js
+++ src/dom/isElement.js
@@ -5,9 +5,5 @@
  * @return {boolean} True if value is a HTMLElement, false otherwise
  */
 export function isElement (val) {
-  if ('HTMLElement' in window) {
-    return (val && val instanceof window.HTMLElement)
-  }
-
   return !!(val && typeof val === 'object' && val.nodeType === 1 && typeof val.nodeName === 'string')
 }
